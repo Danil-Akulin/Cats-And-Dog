56bcb3f461145a3b0f8d1ae2f5ece392b25c5c53
danil akulin <danilakula25@gmail.com>
2021-04-19 13:52:34 +0300

    Commit Cats-And-Dog App

---
+++ CatsAndDogsDB/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace CatsAndDogsDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listPets = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listPetNames = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listPets
+            // 
+            this.listPets.FormattingEnabled = true;
+            this.listPets.Location = new System.Drawing.Point(62, 94);
+            this.listPets.Name = "listPets";
+            this.listPets.Size = new System.Drawing.Size(157, 108);
+            this.listPets.TabIndex = 0;
+            this.listPets.SelectedIndexChanged += new System.EventHandler(this.listPets_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pets";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listPetNames
+            // 
+            this.listPetNames.FormattingEnabled = true;
+            this.listPetNames.Location = new System.Drawing.Point(340, 94);
+            this.listPetNames.Name = "listPetNames";
+            this.listPetNames.Size = new System.Drawing.Size(141, 108);
+            this.listPetNames.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(337, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pet Names";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listPetNames);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listPets);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listPets;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listPetNames;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
